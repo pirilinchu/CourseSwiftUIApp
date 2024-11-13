ec26dd14828c2958c6e602276b343eeb6218c668
Santiago Mendoza <santiagomendozaa6@gmail.com>
2024-11-13 17:45:29 -0400

    Initial Commit

---
+++ Demo/DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoApp.swift
+//  Demo
+//
+//  Created by Santiago Mendoza on 13/11/24.
+//
+
+import SwiftUI
+
+@main
+struct DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
