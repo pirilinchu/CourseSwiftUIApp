7154e83dbdd7d2bb49263ebde54edfdce38d0786
Santiago Mendoza <santiagomendozaa6@gmail.com>
2024-11-13 23:50:46 -0400

    Finish class 18

--- Demo/Views/NListView.swift
+++ Demo/Views/NListView.swift
@@ -10,28 +10,38 @@ import SwiftUI
 struct NListView: View {
     @EnvironmentObject var appInfo: AppInfo
     @State var showCreateSheet: Bool = false
+    @State var selectedCard: NCard?
 
     var body: some View {
-        List {
-            ForEach(appInfo.cards) { card in
-                NCardView(card: card)
-                    .listRowInsets(.none)
-                    .listRowSeparator(.hidden)
-            }
-        }
-        .listStyle(.plain)
-        .sheet(isPresented: $showCreateSheet) {
-            NCreateNoteView { card in
-                appInfo.createCard(card: card)
-                showCreateSheet = false
-            }
-        }
-        .overlay {
-            VStack {
-                Spacer()
-                Button("Create sheet") {
-                    showCreateSheet = true
+        NavigationStack {
+            List {
+                ForEach(appInfo.cards) { card in
+                    Button {
+                        selectedCard = card
+                    } label: {
+                        NCardView(card: card)
+                            .listRowInsets(.none)
+                            .listRowSeparator(.hidden)
+                    }
                 }
+            }
+            .listStyle(.plain)
+            .sheet(isPresented: $showCreateSheet) {
+                NCreateNoteView { card in
+                    appInfo.createCard(card: card)
+                    showCreateSheet = false
+                }
+            }
+            .overlay {
+                VStack {
+                    Spacer()
+                    Button("Create sheet") {
+                        showCreateSheet = true
+                    }
+                }
+            }
+            .navigationDestination(item: $selectedCard) { card in
+                NDetailView(card: card)
             }
         }
     }
